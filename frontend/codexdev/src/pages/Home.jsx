057005640c1c2b3c05ea218cd5d3857a4925d25a
Abdul Rahim <raheemkhan.rk12345@gmail.com>
2026-07-16 17:46:58 +0500

    update rahim

--- frontend/codexdev/src/pages/Home.jsx
+++ frontend/codexdev/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Hero from '../features/home-hero/Hero';
 import Brands from '../features/home-brands/Brands';
 import Services from '../features/home-services/Services';
+import WhyChooseUs from '../features/home-whyChoose/WhyChooseUs';
 
 const Home = () => {
   return (
@@ -10,6 +11,7 @@ const Home = () => {
       <Hero />
       <Brands />
       <Services />
+      <WhyChooseUs />
     </>
   );
 };
